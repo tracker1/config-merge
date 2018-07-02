8ac4a076899e672fb65c1ec12d82d3d548e8cbdb
Michael J. Ryan <mryan@runbeck.net>
2018-07-02 16:50:47 -0700

    save wip

--- src/deep-merge.js
+++ src/deep-merge.js
@@ -1,3 +1,13 @@
+function sortObject(obj) {
+  if (typeof obj !== 'object') return obj;
+  if (typeof obj.length === 'number' && typeof obj.map === 'function') return obj.map(sortObject);
+
+  return Object.keys(obj).sort().reduce((o, k) => {
+    if (obj[k] === undefined) return o;
+    return Object.assign(o, {[k]: sortObject(obj[k])});
+  }, {})
+}
+
 function deepMerge(a, b) {
   if (b === null) return undefined;
   if (b === undefined) return a;
@@ -5,9 +15,7 @@ function deepMerge(a, b) {
   if (typeof a !== 'object') return deepMerge({}, b);
   
   if (b.__REPLACE__) {
-    const ret = {...b};
-    delete ret.__REPLACE__;
-    return ret;
+    return sortObject({ ...b, __REPLACE__: undefined });
   }
 
   return []

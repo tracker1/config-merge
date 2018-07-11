f5253a522a81b9f4cefe0dddc63a2298a299992b
Michael J. Ryan <mryan@runbeck.net>
2018-07-10 17:35:17 -0700

    add jsdoc notes

--- src/util/string-utils.js
+++ src/util/string-utils.js
@@ -1,25 +1,56 @@
 const deepMerge = require('../deep-merge');
 
-// repeatable key-merge
+/**
+ * Wrap up the inheritance change for use with building Strings
+ * @param {object} input Object to properties as last in chain to inherit to
+ * @param {*} filter Method to filter out appropriate keys to work with
+ * @param {*} getBase Method to retrieve appropriate base to inherit from
+ * @returns {object} New/Merged object
+ */
 const parseStrings = (input, filter, getBase) => 
   Object.entries(input)
     .filter(([k]) => filter(k))
     .map(([k, v]) => [k.toLowerCase(), deepMerge(getBase(k.toLowerCase()), v)])
     .reduce((o, [k,v]) => Object.assign(o, { [k]:v }, {}));
 
-// matches language string
+
+/**
+ * Does the input match a language name
+ * @param {string} l
+ * @returns {boolean} is two character language string
+ */
 const matchLang = l => /^[A-Za-z]{2}$/.test(l);
 
 // matches language-locale string
+/**
+ * Does the input match a language-locale string
+ * @param {string} l 
+ * @returns {boolean} is a language-locale string
+ */
 const matchLoc = l => /^[A-Za-z]{2}\-[A-Za-z]{2}$/.test(l);
 
 // gets language part of a string
+/**
+ * Get the language portion of a language/language-locale string
+ * @param {string} l 
+ * @returns {string} the language-portion of the string
+ */
 const lang = l => l.toLowerCase().trim().split('-')[0];
 
-// gets the default value for the input item
+/**
+ * gets the default value for the input item
+ * @param {object} i 
+ * @returns {object} the default property of the input, or an empty object
+ */
 const getDefault = i => i && (i.default || i['!default']) || {};
 
-// creates a merged default value object
+
+/**
+ * creates a merged default value object
+ * @param {object} a Left side to merge
+ * @param {object} b Right side to merge
+ * @returns {object} Merged object defining only a default property
+ */
 const mergeDefault = (a, b) => ({ default: deepMerge(getDefault(a), getDefault(b)) });
 
 // exports all methods
